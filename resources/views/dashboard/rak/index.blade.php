512aa0850246b666cade02f9b0ad81d874d1d6de
ahmdriffai <ahmmd.riffai@gmail.com>
2022-10-10 17:21:38 +0700

    update

--- resources/views/dashboard/rak/index.blade.php
+++ resources/views/dashboard/rak/index.blade.php
@@ -21,23 +21,28 @@
                     <div class="row">
                         @foreach($rak as $data)
                             <div class="col-md-3 mt-3">
-                                <div class="card bg-danger rounded-lg overflow-hidden">
+                                <div class="card bg-danger rounded overflow-hidden">
                                     <div class="card-body p-0 text-center">
                                         <i class="fa fa-book text-white fa-5x mt-4"></i>
                                         <p class="text-white mt-3">{{$data->nama_rak}}</p>
-                                        <a href="{{ route('rak.edit', $data->id) }}">
-                                            <div class="btn btn-info btn-block inline"><i class="fas fa-info"></i></div>
-                                        </a>
-                                        <a href="{{ route('rak.edit', $data->id) }}">
-                                            <div class="btn btn-primary btn-block inline"><i class="fas fa-edit"></i></div>
-                                        </a>
-                                        <form action="{{ route('rak.destroy',$data->id) }}" method="POST" enctype="multipart/form-data">
-                                                @csrf
-                                                @method('delete')
-                                                <button class="btn btn-danger btn-block inline delete-confirm">
-                                                    <i class="fas fa-trash"></i>
-                                                </button>
-                                        </form>
+                                        <div class="btn-group btn-group-toggle" data-toggle="buttons" style="width:100%;">
+                                            <div class="btn btn-success">
+                                                <a href="{{ route('buku.show', $data->id) }}" class="btn"><i class="fas fa-plus text-white"></i></a>
+                                            </div>
+                                            <div class="btn btn-info">
+                                                <a href="{{ route('rak.show', $data->id) }}" class="btn"><i class="fas fa-info text-white"></i></a>
+                                            </div>
+                                            <div class="btn btn-primary">
+                                                <a href="{{ route('buku.show', $data->id) }}" class="btn"><i class="fas fa-edit text-white"></i></a>
+                                            </div>
+                                            <form action="{{ route('rak.destroy',$data->id) }}" method="POST" enctype="multipart/form-data" class="btn btn-danger">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <button class="btn delete-confirm">
+                                                        <i class="fas fa-trash text-white"></i>
+                                                    </button>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -51,3 +56,17 @@
         </div>
     </div>
 @endsection
+
+<a href="{{ route('rak.edit', $data->id) }}">
+    <div class="btn btn-info btn-block inline"><i class="fas fa-info"></i></div>
+</a>
+<a href="{{ route('rak.edit', $data->id) }}">
+    <div class="btn btn-primary btn-block inline"><i class="fas fa-edit"></i></div>
+</a>
+<form action="{{ route('rak.destroy',$data->id) }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        @method('delete')
+        <button class="btn btn-danger btn-block inline delete-confirm">
+            <i class="fas fa-trash"></i>
+        </button>
+</form>
